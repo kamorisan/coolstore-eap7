feda406701872bffc0570d53a2ff88ff8b10a2f1
kamori <kamori@redhat.com>
2026-07-16 17:46:05 +0900

    Copy JMS Topic CLI script to extensions directory
    The configure.cli script needs to be copied to $JBOSS_HOME/extensions/
    so that EAP will execute it during startup.

--- s2i/eap-install/install.sh
+++ s2i/eap-install/install.sh
@@ -12,4 +12,10 @@ install_modules "${INJECTED_DIR}/modules"
 echo "Configuring PostgreSQL JDBC driver"
 configure_drivers "${INJECTED_DIR}/drivers.env"
 
+echo "Configuring JMS Topic"
+if [ -f "${INJECTED_DIR}/extensions/configure.cli" ]; then
+    cp "${INJECTED_DIR}/extensions/configure.cli" "${JBOSS_HOME}/extensions/"
+    echo "JMS Topic configuration script copied to ${JBOSS_HOME}/extensions/"
+fi
+
 echo "PostgreSQL JDBC driver installation completed"
